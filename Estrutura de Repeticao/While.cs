a03b2790e78fd9dcb404a7d0d746a23f647b6a0f
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-06-26 12:18:31 -0300

    Create While.cs

---
+++ Estrutura de Repeticao/While.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int cont =1;
+		
+		while ( cont < 5 ) {
+			
+			Console.WriteLine("O valor do contador é: " + cont);
+			cont++;
+			
+		} 
+	}
+}
