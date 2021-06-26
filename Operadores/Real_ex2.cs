61245e5bd5b13974ae32a1b395f43476e4c7d1d7
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-06-26 12:25:18 -0300

    Create Real_ex2.cs

---
+++ Operadores/Real_ex2.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Real_ex2
+{
+	public static void Main()
+{
+            double num;
+
+            Console.Write("Entre com um numero real: ");
+		
+            num = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O numero inserido por você foi: " + num);
+        }
+}
