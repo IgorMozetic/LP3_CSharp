9f1bea56cf0e5010d794b5060109acadd3265c97
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-06-26 12:14:30 -0300

    Create Operadores.cs

---
+++ Operadores/Operadores.cs
@@ -0,0 +1,25 @@
+using System;
+					
+public class Operadores
+{
+	public static void Main()
+	{
+		double a = 5, b = 2;
+		double resultado;
+		
+		resultado = a + b;
+		Console.WriteLine(resultado);
+		 
+		resultado = a - b;
+		Console.WriteLine(resultado);
+		
+		resultado = a * b;
+		Console.WriteLine(resultado);
+		
+		resultado = a / b;
+		Console.WriteLine(resultado);
+		
+		resultado = a % b;
+		Console.WriteLine(resultado);
+	}
+}
