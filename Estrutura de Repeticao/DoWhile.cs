775d5446a4efc433c3189060bb059473837cff49
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-06-26 12:18:56 -0300

    Create DoWhile.cs

---
+++ Estrutura de Repeticao/DoWhile.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class DoWhile
+{
+	public static void Main()
+	{
+		int cont =1;
+		
+		do {
+			
+			Console.WriteLine("O valordod contador é: " + cont);
+			cont++;
+			
+		} while ( cont < 5 ); 
+	}
+}
