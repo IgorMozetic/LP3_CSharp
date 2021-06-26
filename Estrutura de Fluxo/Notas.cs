6b3c38fdb5dbb621703cc8b92cb7ac11f15b558f
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-06-26 12:29:51 -0300

    Create Notas.cs

---
+++ Estrutura de Fluxo/Notas.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Notas
+{
+	public static void Main()
+	{
+		double nota, nota2;
+		Console.Write("Entre com dois número inteiros: ");
+		nota = int.Parse(Console.ReadLine());
+		nota2 = int.Parse(Console.ReadLine());
+		
+		if ( (nota >= 0 && nota <= 10) && (nota2 >= 0 && nota2 <= 10)  )
+			Console.Write("A média entre as notas é: " + ((nota + nota2) / 2) );
+		else 
+			Console.WriteLine("Você inseriu nontas inválidas!");
+	}
+}
