d572acbb2347d5dce0d15915581ff8f6d3105a7b
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-06-26 12:25:00 -0300

    Create Inteiro_ex1.cs

---
+++ Operadores/Inteiro_ex1.cs
@@ -0,0 +1,11 @@
+using System;
+public class Inteiro_ex1
+{
+  public static void Main()
+{
+    int num;
+    Console.Write("Entre com um numero inteiro: ");
+    num = int.Parse(Console.ReadLine());
+    Console.WriteLine("O numero inserido por você foi: " + num);
+  }
+}
