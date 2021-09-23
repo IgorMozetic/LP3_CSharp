d24f6452d86430896459ebc96c8427232e03afa0
Igor Mozetic <imozetic@blueedtech.com.br>
2021-09-22 22:58:55 -0300

    ATV12

---
+++ POO - Orientação a Objeto/Atividade_aula12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Atividade_aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quad1 = new Quadrado(5.0);
+            Quadrado quad2 = new Quadrado("Laranja", 6.0);
+
+            Retangulo retan1 = new Retangulo(10.0, 5.0);
+            Retangulo retan2 = new Retangulo("Preto",10.0,6.0);
+
+            Circulo circ1 = new Circulo(5.0);
+            Circulo circ2 = new Circulo("Vermelho", 6.0);
+
+            Triangulo tri1 = new Triangulo(10.0, 5.0);
+            Triangulo tri2 = new Triangulo("Cinza", 10.0, 6.0);
+        }
+    }
+}
---
+++ POO - Orientação a Objeto/Atividade_aula12/Quadrado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_aula12
+{
+    class Quadrado : Forma 
+    {
+        private double lado;
+
+        public Quadrado(double lado)
+        {
+            base.setCor("Azul");
+            this.lado = lado;
+            Console.WriteLine("Área do Quadrado: " + this.calculcarArea() + "\n");
+        }
+
+        public Quadrado(string cor, double lado)
+        {
+            base.setCor(cor);
+            this.lado = lado;
+            Console.WriteLine("Área do Quadrado: " + this.calculcarArea() + "\n");
+        }
+        public override double calculcarArea()
+        {
+            return lado * lado;
+        }
+    }
+}
