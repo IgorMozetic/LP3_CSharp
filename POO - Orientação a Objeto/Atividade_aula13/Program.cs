416a0fa80f416f58b26fa8a01aa839fb41208ba4
Igor Mozetic <imozetic@blueedtech.com.br>
2021-09-29 23:25:38 -0300

    ATV13

---
+++ POO - Orientação a Objeto/Atividade_aula13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade_aula13
+{
+    class Program
+    {
+        static void mostrarArea(IForma forma)
+        {
+            Console.WriteLine("Área a forma: " + forma.calcularArea());
+        }
+        static void Main(string[] args)
+        {
+            IForma forma = new Quadrado(10.0);
+            mostrarArea(forma);
+
+            forma = new Retangulo(5.0, 7.5);
+            mostrarArea(forma);
+
+            forma = new Circulo(5.0);
+            mostrarArea(forma);
+
+            forma = new Triangulo(5.0, 7.5);
+            mostrarArea(forma);
+        }
+    }
+}
